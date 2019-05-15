ba0bc55d5bd11d22438fc76ef416146dd80301be
Jaclyn <jaclyndshahan@gmail.com>
2019-05-15 18:54:28 -0500

    working on contact section

--- src/Contact.js
+++ src/Contact.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-
+import './Contact.css';
+import {Input, Divider} from 'antd';
 
 class Contact extends Component {
     constructor() {
@@ -12,6 +13,24 @@ class Contact extends Component {
     render() {
         return (
             <div>
+                <form className="form">
+                <header>
+                    <h1>Please feel free to contact me</h1>
+                    <Divider/>
+                    <h3>Enter your info below to contact me. All messages get immediatly sent to my inbox.</h3>
+                    <Divider/>
+                </header>
+                <div className="inputBox">
+                    <Input className="inputName" size="large" placeholder="Name" />
+                
+                    <Input className="inputName" size="large" placeholder="Email" />
+                    <form placeholder="Leave me message" className="message">
+
+                    </form>
+                </div>
+
+
+                </form>
 
             </div>
         )
