67b8c59465effbe9f502e58ead84f4c24340e8ba
Jaclyn <jaclyndshahan@gmail.com>
2019-05-15 20:01:32 -0500

    added link to github and changed css on contact section

--- src/Contact.js
+++ src/Contact.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import './Contact.css';
-import {Input, Divider} from 'antd';
+import {Input, Divider, Button} from 'antd';
 
 class Contact extends Component {
     constructor() {
@@ -11,6 +11,7 @@ class Contact extends Component {
     }
 
     render() {
+        const { TextArea } = Input;
         return (
             <div>
                 <form className="form">
@@ -21,15 +22,19 @@ class Contact extends Component {
                     <Divider/>
                 </header>
                 <div className="inputBox">
-                    <Input className="inputName" size="large" placeholder="Name" />
+                    <Input className="inputName inputClass" size="large" placeholder="Name" />
                 
-                    <Input className="inputName" size="large" placeholder="Email" />
-                    <form placeholder="Leave me message" className="message">
-
-                    </form>
+                    <Input className="inputName inputClass" size="large" placeholder="Email" />
+                    <TextArea rows={4} placeholder="Leave me message..." className="message inputClass" />
+                    <br></br>
+                    <Button type="primary">Submit</Button>
                 </div>
-
-
+                <footer>
+                    <a href="https://github.com/JaclynShahan" target="_blank">
+                    <img className="imageSize" src={require("./images/GitHubIcon.jpg")}/>
+                    </a>
+                </footer>
+                    
                 </form>
 
             </div>
