1b947deeac1173960ded3f3b491fccfc46466b0a
Jaclyn <jaclyndshahan@gmail.com>
2019-05-20 14:50:56 -0500

    working on about page

--- src/About/About.js
+++ src/About/About.js
@@ -14,13 +14,21 @@ class About extends Component {
           const { dotPosition } = this.state;
         return (
             <div className="backgroundClass">
- 
+            <div>
+              <h2 className="aboutForm">
+                My name is Jaclyn Shahan. I am a motivated learner hoping to transfer from Management and Billing to a career in full-stack web development. 
+                I have experience with React, Redux, Router, Node/Express, SQL/PostgreSQl, and RESTful API's.
+                I am a proud mother of 3 with a supportive family and in my spare time I love to paint and do DIY activities.
+                I cannot wait to begin my journey as a web developer and look forward to learning and growing in this field.
+             </h2>
+               </div>
+                <form className="carouselForm clearColor">
             <Carousel autoplay className="carousel" dotPosition={dotPosition}>
             <div>
               <img className="images" src="https://scontent-dfw5-2.xx.fbcdn.net/v/t1.0-9/60150255_10218163437853379_8579273649947672576_n.jpg?_nc_cat=103&_nc_ht=scontent-dfw5-2.xx&oh=241e6e21a0d9a70441e8d212092b93a7&oe=5D5A55EA" />
             </div>
             <div>
-              <img className="images" src="https://scontent-dfw5-2.xx.fbcdn.net/v/t1.0-9/49938933_10217220651924320_6741260078004830208_n.jpg?_nc_cat=100&_nc_ht=scontent-dfw5-2.xx&oh=cdf70d1f03e1da98dc4357e6cecca6c8&oe=5D5B1DCC" />
+              <img className="images" src={require("./FamilyPic.jpg")} />
             </div>
             <div>
               <img className="images" src="https://scontent-dfw5-2.xx.fbcdn.net/v/t1.0-9/50278760_10217318370567225_4661217857617526784_n.jpg?_nc_cat=101&_nc_ht=scontent-dfw5-2.xx&oh=13157b08332699f2dea6a91758b24a6e&oe=5D6347AE" />
@@ -29,10 +37,12 @@ class About extends Component {
               <img className="images" src="https://scontent-dfw5-2.xx.fbcdn.net/v/t1.0-9/56214361_10217858761556662_5831365101659095040_n.jpg?_nc_cat=111&_nc_ht=scontent-dfw5-2.xx&oh=b64229ac1102374a0ee31a0d136776a8&oe=5D51DB61"/>
             </div>
           </Carousel>
-          <br></br>
-          <footer>
-              <h2 className="resume">View/Download my resume here: <a href="https://docs.google.com/document/d/1p_9mD2PBjQ7gIAadS4KfPKhjNprF_iKQKQCBczu0_TY/edit" target="_blank"><Icon className="fileIcon" type="file"/> </a> </h2>
-          </footer>
+          </form>
+          <div>
+          <h2 className="resume">View/Download my resume here: <a href="https://docs.google.com/document/d/1p_9mD2PBjQ7gIAadS4KfPKhjNprF_iKQKQCBczu0_TY/edit" target="_blank"><Icon className="fileIcon" type="file"/> </a> </h2>
+          </div>
+        
+          
          </div>
         )
     }
